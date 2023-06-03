4c4b688df29bee78c436f6e1ab6f4e905af58087
codewithadil <adilalam2402@gmail.com>
2023-06-03 23:36:46 +0530

    first calculator python program

---
+++ calculator.py
@@ -0,0 +1,42 @@
+# How to create a calulator using python
+
+def add (a,b):
+      # This function is used for adding two numbers  
+    return a+b
+def subtract (a,b):
+    # This function is used for subtracting two numbers   
+    return a-b
+def multiply (a,b):
+    # This function is used for multiplying two numbers 
+    return a*b
+def divide (a,b):
+     # This function is used for dividing two numbers  
+    return a/b
+def floor_division(a,b):
+     # This function is used for floor division  
+    return a//b
+
+print("Select an operation to perform:")
+print("1. ADD")
+print("2. SUBTRACT")
+print("3. MULTIPLY")
+print("4. DIVIDE")
+print("5. FLOOR DIVISION")
+
+operation = input()
+
+num1 = int(input("Please Enter first no. "))
+num2 = int(input("Please Enter second no. "))
+
+if operation == "1":
+    print(num1, "+", num2, "=", add(num1,num2))
+elif operation == "2":
+    print(num1, "-", num2, "=", subtract(num1,num2))
+elif operation == "3":
+    print(num1, "*", num2, "=", multiply(num1,num2))
+elif operation == "4":
+    print(num1, "/", num2, "=", divide(num1,num2))
+elif operation == "5":
+    print(num1, "//", num2, "=", floor_division(num1,num2))
+else:
+    print("invalid input")
